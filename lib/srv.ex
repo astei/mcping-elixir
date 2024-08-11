70fc1d43464cc7d3062fcdb14d454c8524526cda
Andrew Steinborn <git@steinborn.me>
2024-08-11 13:29:57 -0400

    Extend `MCPing.Srv` to support returning multiple servers as well

--- lib/srv.ex
+++ lib/srv.ex
@@ -1,39 +1,66 @@
 defmodule MCPing.Srv do
   @moduledoc """
-  Functions for resolving SRV records.
+  Functions for resolving SRV records. SRV records are used to provide a way to map a service name and protocol to a hostname and port.
+  This module is used as a part of MCPing for optionally resolving the SRV record for a Minecraft server, but it is general enough to
+  be used for any service that uses SRV records, such as XMPP or SIP.
   """
 
   @doc """
-  Resolves a SRV record for a given service, protocol, and hostname.
+  Resolves a single SRV record for a given service, protocol, and hostname. Returns the first SRV record with the
+  highest priority. If no SRV records are found, returns an error.
 
   ## Examples
 
       iex> MCPing.Srv.resolve_srv_record("minecraft", "tcp", "hypixel.net", 5000)
-      {~c"mc.hypixel.net", 25565}
+      {:ok, {~c"mc.hypixel.net", 25565}}
+
+      iex> MCPing.Srv.resolve_srv_record("minecraft", "tcp", "mc.example.com", 5000)
+      {:error, :nxdomain}
   """
   def resolve_srv_record(service, protocol, hostname, timeout) do
+    case lookup_server(service, protocol, hostname, timeout) do
+      {:ok, entries} -> {:ok, hd(entries)}
+      {:error, err} -> {:error, err}
+    end
+  end
+
+  @spec lookup_server(binary(), binary(), binary(), :infinity | non_neg_integer()) ::
+          {:error, atom()}
+  @doc """
+  Resolves all SRV records for a given service, protocol, and hostname. Returns a list of all SRV records found,
+  sorted by priority order. If a record with a given priority has multiple records with different weights, the
+  records will be randomly selected based on their weight.
+
+  If no SRV records are found, returns `:error`.
+
+  ## Examples
+
+      iex> MCPing.Srv.lookup_server("minecraft", "tcp", "hypixel.net", 5000)
+      {:ok, [{~c"mc.hypixel.net", 25565}]}
+  """
+  def lookup_server(service, protocol, hostname, timeout) do
     minecraft_srv = to_charlist("_" <> service <> "._" <> protocol <> "." <> hostname)
 
     case :inet_res.getbyname(minecraft_srv, :srv, timeout) do
-      {:ok, {:hostent, _, _, :srv, _, records}} -> {:ok, find_first_eligible_srv_record(records)}
-      _ -> nil
+      {:ok, {:hostent, _, _, :srv, _, records}} -> {:ok, find_eligible_srv_records(records)}
+      {:error, err} -> {:error, err}
     end
   end
 
   @doc false
-  def find_first_eligible_srv_record(records) when is_list(records) and length(records) == 1 do
+  def find_eligible_srv_records(records) when is_list(records) and length(records) == 1 do
     {_, _, port, host} = hd(records)
-    {host, port}
+    [{host, port}]
   end
 
   @doc false
-  def find_first_eligible_srv_record(records) when is_list(records) do
-    {t, _} = find_first_eligible_srv_record(records, :rand.seed_s(:default))
+  def find_eligible_srv_records(records) when is_list(records) do
+    {t, _} = find_eligible_srv_records(records, :rand.seed_s(:default))
     t
   end
 
   @doc false
-  def find_first_eligible_srv_record(records, rand_state) do
+  def find_eligible_srv_records(records, rand_state) do
     picked_by_priority =
       records
       |> Enum.sort_by(fn {priority, weight, _, _} -> {priority, -weight} end)
@@ -41,10 +68,18 @@ defmodule MCPing.Srv do
         {weight, port, host}
       end)
 
-    max_priority = Map.keys(picked_by_priority) |> Enum.min()
-    weighted = Map.get(picked_by_priority, max_priority)
+    # Since Erlang maps are unordered, we need to sort the keys to ensure that we always pick the same order of priorities.
+    sorted_priorities = picked_by_priority
+      |> Map.keys()
+      |> Enum.sort()
 
-    pick_weighted_random_s(weighted, rand_state)
+    {selected_record_by_priority_reversed, rand_state} = Enum.reduce(sorted_priorities, {[], rand_state}, fn
+      priority, {entries, rand_state} ->
+        {entry, rand_state} = pick_weighted_random_s(picked_by_priority[priority], rand_state)
+        {[entry | entries], rand_state}
+    end)
+
+    {Enum.reverse(selected_record_by_priority_reversed), rand_state}
   end
 
   defp pick_weighted_random_s(entries, rand_state) do
--- test/srv_test.exs
+++ test/srv_test.exs
@@ -2,15 +2,15 @@ defmodule MCPing.SrvTest do
   use ExUnit.Case, async: true
 
   test "single SRV records work" do
-    assert MCPing.Srv.find_first_eligible_srv_record([{1, 1, 25565, "example.com"}]) ==
-             {"example.com", 25565}
+    assert MCPing.Srv.find_eligible_srv_records([{1, 1, 25565, "example.com"}]) ==
+             [{"example.com", 25565}]
   end
 
   test "will pick the SRV record with the lowest priority first" do
-    assert MCPing.Srv.find_first_eligible_srv_record([
+    assert MCPing.Srv.find_eligible_srv_records([
              {1, 1, 25566, "s2.example.com"},
              {0, 1, 25565, "s1.example.com"}
-           ]) == {"s1.example.com", 25565}
+           ]) == [{"s1.example.com", 25565}, {"s2.example.com", 25566}]
   end
 
   test "will respect SRV record weight" do
@@ -26,15 +26,63 @@ defmodule MCPing.SrvTest do
     results =
       Enum.map(1..100, fn _ ->
         {v, state} =
-          MCPing.Srv.find_first_eligible_srv_record(records, Process.get(:srv_test_state))
+          MCPing.Srv.find_eligible_srv_records(records, Process.get(:srv_test_state))
 
         Process.put(:srv_test_state, state)
         v
       end)
 
     assert Enum.count(results, fn
-             {"s1.example.com", 25565} -> true
+             [{"s1.example.com", 25565}] -> true
              _ -> false
            end) in 20..30
+  end
+
+  test "will respect SRV record weights for multiple priorities" do
+    records = [
+      {1, 25, 25565, "s1.example.com"},
+      {1, 75, 25566, "s2.example.com"},
+      {0, 50, 25567, "s3.example.com"},
+      {0, 50, 25568, "s4.example.com"}
+    ]
+    state = :rand.seed_s(:exsss, 1)  # different seed, which makes the test pass
+    Process.put(:srv_test_state, state)
+
+    # At priority 0, we get either s3 or s4 50% of the time, and at priority 1, we get s1 25% of the time and s2 75% of the time.
+    # We will get each combination with the following probabilities:
+    # - P(s3, s1) = .5 * .25 = .125,
+    # - P(s4, s2) = .5 * .75 = .375
+    # - P(s3, s2) = .5 * .75 = .375
+    # - P(s4, s1) = .5 * .25 = .125
+
+    # We'll run this test 100 times to ensure that the distribution is roughly correct.
+    results =
+      Enum.map(1..100, fn _ ->
+        {v, state} =
+          MCPing.Srv.find_eligible_srv_records(records, Process.get(:srv_test_state))
+
+        Process.put(:srv_test_state, state)
+        v
+      end)
+
+    assert Enum.count(results, fn
+             [{"s3.example.com", 25567}, {"s1.example.com", 25565}] -> true
+             _ -> false
+           end) in 10..20
+
+    assert Enum.count(results, fn
+             [{"s4.example.com", 25568}, {"s2.example.com", 25566}] -> true
+             _ -> false
+           end) in 30..40
+
+    assert Enum.count(results, fn
+             [{"s3.example.com", 25567}, {"s2.example.com", 25566}] -> true
+             _ -> false
+           end) in 30..40
+
+    assert Enum.count(results, fn
+             [{"s4.example.com", 25568}, {"s1.example.com", 25565}] -> true
+             _ -> false
+           end) in 10..20
   end
 end
